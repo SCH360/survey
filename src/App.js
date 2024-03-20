22110b149e7976008e3101f88f2cf5333f0b8f29
SCH360 <143849066+SCH360@users.noreply.github.com>
2024-03-20 15:44:55 -0700

    Update - 3/20/24

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import './style.css';
+import axios from 'axios';
 import React, {useState} from 'react';
 import {TextInput, View} from 'react-native';
 import WelcomeImage from './Assets/Welcome.png'
 
 function App() {
+  const[SelectedState, setSelectedState] = useState("");
+  const[day,SetDay] = useState("");
+  const[month,SetMonth] = useState("");
+  const[year,SetYear] = useState("");
+  const[FirstName, SetFirstName] = useState("");
+  const[LastName, SetLastName] = useState("");
+  const[Email, SetEmail] = useState("");
+  const[Gender, SetGender] = useState("");
+  const apiEndPoint = "http://0.0.0.0:5000/api";
   const[isGenderBox2,setisGenderBox2] = useState(false);
   const[isGenderBox3,setisGenderBox3] = useState(false);
   const[isGenderBox4,setisGenderBox4] = useState(false);
@@ -15,7 +25,6 @@ function App() {
   };
   const buttonClick3 = ()=>{setisGenderBox4(!isGenderBox4);
   };
-  const buttonClick4 = ()=>{}
   const states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
   'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
   'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan',
@@ -23,19 +32,11 @@ function App() {
   'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma',
   'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas',
   'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
-  const[SelectedState, setSelectedState] = useState("");
-  const[day,SetDay] = useState("");
-  const[month,SetMonth] = useState("");
-  const[year,SetYear] = useState("");
   const days = Array.from({length:31},(_,i)=>i+1);
   const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
   const currentYear=new Date().getFullYear();
   const years = Array.from({length:100},(_,i)=>currentYear-i);
   const value = currentYear-year;
-  const HandleDayChange = (event) => {SetDay(event.target.value)};
-  const HandleMonthChange = (event) => {SetMonth(event.target.value)};
-  const HandleYearChange = (event) => {SetYear(event.target.value)};
-  const HandleStateChange = (e) => {setSelectedState(e.target.value)};
   const questions = [
     {
       question:"What is your ethnicity/race?",
@@ -63,6 +64,17 @@ const ReferQuestions = [
 const[CurrentQuestion, setCurrentQuestion] = useState(0);
 const [answers, setAnswers] = useState([]);
 const HandleAnswer = (answer) => {};
+const HandleSurvey = async() => {
+  try{
+    const response = await axios.post(`${apiEndPoint}/surveys`, {
+    FirstName, LastName, Email, Gender, month, day, year, state:SelectedState
+    });
+    console.log("Survey Response", response.data);
+  }
+  catch(error){
+    console.error("Error Submitting Survey", error.message);
+  }
+}
   return (
     
     <>
@@ -77,17 +89,17 @@ const HandleAnswer = (answer) => {};
     </div>
 
       <div className="FirstName">
-      <input className = "BiggerText" type="text" placeholder = " Type Here..."/>
+      <input className = "BiggerText" type="text" placeholder = " Type Here..." value = {FirstName} onChange = {(e) => SetFirstName(e.target.value)}/>
         </div>
       {/* This is the Last Name Question */}
       <p className="LastNameText">Last Name</p>
       <div className="LastName">
-      <input className = "BiggerText" type="text" placeholder = " Type Here..."/>
+      <input className = "BiggerText" type="text" placeholder = " Type Here..." value = {LastName} onChange = {(e)=> SetLastName(e.target.value)}/>
       </div>
       {/* This is the Email Address Question */}
       <p className="EmailText">Email Address</p>
       <div className="Email">
-      <input className = "BiggerText" type="text" placeholder = " Type Here..."/>
+      <input className = "BiggerText" type="text" placeholder = " Type Here..." value = {Email} onChange = {(e) => SetEmail(e.target.value)}/>
       </div>
       {/* This is the gender question */}
       <div className="GenderText">
@@ -97,10 +109,10 @@ const HandleAnswer = (answer) => {};
       </div>
       {/* These are the gender buttons */}
       <View>
-        <div className={isGenderBox2 ?"GenderBox2":"GenderBox"}><button className = "GenderButtonStyle" onClick = {buttonClick}>Male</button></div></View>
+        <div className={isGenderBox2 ?"GenderBox2":"GenderBox"}><button className = "GenderButtonStyle" onClick = {()=>SetGender("Male")}>Male</button></div></View>
         
       <View>
-      <div className={isGenderBox3 ?"GenderBox2":"GenderBox"}><button className = "GenderButtonStyle" onClick = {buttonClick2}>Female</button></div></View>
+      <div className={isGenderBox3 ?"GenderBox2":"GenderBox"}><button className = "GenderButtonStyle" onClick = {()=>SetGender("Female")}>Female</button></div></View>
       
       
       {/* This is the Date of Birth Question */}
@@ -108,17 +120,17 @@ const HandleAnswer = (answer) => {};
         <h2>What is your Date of Birth?</h2>
         <div className = "DateofBirthButton">
           {/* This is the Date of Birth Buttons */}
-          <select className = "SelectText" value = {month}onChange = {HandleMonthChange}>
+          <select className = "SelectText" value = {month}onChange = {(e)=>SetMonth(e.target.value)}>
             <option value="">Month</option>
             {months.map((m)=>(<option key = {m}value = {m}>{m}</option>))}
           </select>
           <label>  /  </label>
-          <select className = "SelectText" value = {day}onChange = {HandleDayChange}>
+          <select className = "SelectText" value = {day}onChange = {(e)=>SetDay(e.target.value)}>
             <option value="">Day</option>
             {days.map((d)=>(<option key = {d}value = {d}>{d}</option>))}
           </select>
           <label>  /  </label>
-          <select className = "SelectText" value = {year}onChange = {HandleYearChange}>
+          <select className = "SelectText" value = {year}onChange = {(e)=>SetYear(e.target.value)}>
             <option value="">Year</option>
             {years.map((y)=>(<option key = {y}value = {y}>{y}</option>))}
           </select>
@@ -142,7 +154,7 @@ const HandleAnswer = (answer) => {};
       {/* These are the state options */}
       <div className="StateDropdownElement">
         <label htmlFor = "State Dropdown"></label>
-        <select className = "StateDropdownBox" id = "State Dropdown" value = {SelectedState} onChange = {HandleStateChange}>
+        <select className = "StateDropdownBox" id = "State Dropdown" value = {SelectedState} onChange = {(e)=>setSelectedState(e.target.value)}>
           <option value = "">Select State</option>
           {states.map((state, index)=>(
             <option key = {index}value = {state}>{state}</option>
